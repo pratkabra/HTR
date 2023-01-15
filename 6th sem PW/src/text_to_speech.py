6ab0516902d7e3804cc82d960afe23ffeed507f6
Prateek Kabra <54915285+pratkabra@users.noreply.github.com>
2023-01-15 14:45:03 +0530

    Add files via upload
    Uploaded all files relevant to HTR Project

---
+++ 6th sem PW/src/text_to_speech.py
@@ -0,0 +1,14 @@
+# Import the Gtts module for text  
+# to speech conversion 
+from gtts import gTTS 
+# import Os module to start the audio file
+import os 
+fh = open('/content/drive/MyDrive/6th sem PW/src/text.txt', 'r')
+myText = fh.read().replace("\n", " ")
+# Language we want to use 
+language = 'en'
+output = gTTS(text=myText, lang=language, slow=False)
+output.save("output.mp3")
+#fh.close()
+# Play the converted file 
+os.system('start output.mp3')
